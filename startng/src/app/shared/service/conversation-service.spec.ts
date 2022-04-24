a9a4694cbc191b5095efe6e10050cfe948f86295
Mariem SOUISSI <souissimariem111@gmail.com>
2022-04-24 14:53:11 +0100

    Commit Services Mariem

--- startng/src/app/shared/service/conversation-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ConversationService } from './conversation-service';
-
-describe('ConversationService', () => {
-  it('should create an instance', () => {
-    expect(new ConversationService()).toBeTruthy();
-  });
-});
--- startng/src/app/shared/service/offer-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { OfferService } from './offer-service';
-
-describe('OfferService', () => {
-  it('should create an instance', () => {
-    expect(new OfferService()).toBeTruthy();
-  });
-});
